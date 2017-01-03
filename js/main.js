77ead9316b4b52bf5302b1a55333fae5fff4647e
likunlang <likunlang@foxmail.com>
2017-01-03 17:49:25 +0800

    no message

--- js/main.js
+++ js/main.js
@@ -27,6 +27,7 @@ $WIN.on('load', function() {
 
     // will fade out the whole DIV that covers the website.
     $("#preloader").delay(200).fadeOut("slow");
+    //isotope显示图片重置
     $("html,body").resize();
 
 
